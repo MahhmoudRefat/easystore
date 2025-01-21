0a15034a5713a3c7ac1891804f8367f98bdc1f5b
mahhmoudrefat <mahhmoudrefat@outlook.com>
2025-01-21 10:57:09 +0200

    Update AllproductsServices add AllCategoriesService add CategoriesService

--- lib/services/get_all_product_service.dart
+++ lib/services/get_all_product_service.dart
@@ -1,19 +1,18 @@
 import 'dart:convert';
 
+import 'package:http/http.dart%20';
+import 'package:projects/helper/api.dart';
 import 'package:projects/models/product_model.dart';
-import 'package:http/http.dart' as http;
 
-class AllproductsServices{
-Future<List<ProductModel>> getAllProducts() async{
-  http.Response response = await http.get(Uri.parse("https://fakestoreapi.com/products"));
-  List<dynamic> data = jsonDecode(response.body);
-  List<ProductModel> productList = [];
-  for(int i = 0 ; i < data.length;i++){
+class AllproductsServices {
+  Future<List<ProductModel>> getAllProducts() async {
+    List<dynamic> data = await Api().get(url: "https://fakestoreapi.com/products");
+
+    List<ProductModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
 //ProductModel.fromJson(data[i]);
-productList.add(ProductModel.fromJson(data[i])); // تضيف المنتج هنا
-
-}
-  return productList;
-}
-
+      productList.add(ProductModel.fromJson(data[i])); // تضيف المنتج هنا
+    }
+    return productList;
+  }
 }
